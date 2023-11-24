a37f6b95c30e18daf5d0a096e975d10b3ffda169
8aChristian <christian88afu@gmail.com>
2023-11-24 11:38:53 -0500

    comentario

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appFinance',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
